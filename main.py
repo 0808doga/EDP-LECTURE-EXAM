cafda910dc4e67c541427cc5c529885f82223312
Doğa Şimşek <148654282+0808doga@users.noreply.github.com>
2025-01-23 15:57:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -48,9 +48,12 @@ if __name__ == "__main__":
     logger = Logger()
     notifier = Notifier()
 
+
     sensor.on("data_detected", processor.process)
     processor.on("data_processed", logger.log)
     processor.on("data_error", notifier.notify)
+
+    print("Simulating events...\n")
 
     sensor.detect("Temperature: 25°C")
     sensor.detect("Error: Sensor malfunction")
